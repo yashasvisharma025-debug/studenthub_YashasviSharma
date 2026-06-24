12608e60a01ea67f0236eaec949c29c83cba29be
Yashasvi <yashasvisharma025@gmail.com>
2026-06-24 14:26:00 +0530

    Made Sticky Notes

--- script.js
+++ script.js
@@ -1,3 +1,71 @@
 let tasks= JSON.parse(localStorage.getItem('tasks')) || [];
 let notes=JSON.parse(localStorage.getItem('notes')) || [];
 
+const taskInput=document.getElementById('taskInput');
+const addBtn=document.getElementById('addBtn');
+const taskList=document.getElementById('tasklist');
+
+const notesContainer = document.getElementById('notesContainer');
+const addNoteBtn = document.getElementById('addNoteBtn');
+
+function renderTasks() {
+    tasksList.innerHTML = '';
+    tasks.forEach((taskText, index) => {
+        const li=document.createElement('li');
+        li.className="flex justify-between items-center p-2 border-b border-secondary";
+        li.innerHTML=`
+            <span>${taskText}</span>
+            <button class="del-btn text-red-500 font-bold" data-index="${index}">Delete</button>
+        `;
+        taskList.appendChild(li);
+    });
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+taskList.addEventListener('click', (e) =>{
+    if (e.target.classList.contains('del-btn')) {
+        const index=e.target.getAttribute('data-index');
+        tasks.splice(index, 1);
+        renderTasks();
+    }
+});
+
+addBtn.addEventListener('click', () => {
+    if (taskInput.value.trim() !== "") {
+        tasks.push(taskInput.value);
+        taskInput.value = '';
+        renderTasks();
+    }
+});
+
+function renderNotes(){
+    notesContainer.innerHTML='';
+    notes.forEach((note, index) => {
+        const div=document.createElement('div')
+;
+    div.className="p-4 bg-white border-2 border-secondary rounded-lg shadow-sm";
+    div.innerHTML=`
+    <h3 class="font-bold text-primary">${note.title}</h3>
+    <p class= "text-sm text-gray-700 mt-1">${note.body}</p>
+    <button oneclick="deleteNote(${index})" class="text-xs text-red-500 underline mt-3">Delete</button>
+    `;
+notesContainer.appendChild(div);});
+localStorage.setItem('notes', JSON.stringify(notes));
+}
+
+function deleteNote(index){
+    notes.splice(index, 1);
+    renderNotes();
+}
+
+addNoteBtn.addEventListener('click', () => {
+    const title = prompt("Note Ttitle:");
+    const body = prompt("Note Body:");
+    if (title && body) {
+        notes.push({ title, body });
+        renderNotes();
+    }
+});
+
+renderTasks();
+renderNotes();
